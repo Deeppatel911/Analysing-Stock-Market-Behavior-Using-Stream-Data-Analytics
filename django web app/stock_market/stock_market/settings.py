16ac4c6cdc139be9b9098a3a6d21b4e999a45a5d
Deeppatel911 <deepjpatel0911@gmail.com>
2022-05-19 00:42:16 +0530

    Update settings.py

--- django web app/stock_market/stock_market/settings.py
+++ django web app/stock_market/stock_market/settings.py
@@ -142,7 +142,7 @@ LOGIN_URL = 'login'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'SMT_EMAIL'
+EMAIL_HOST = 'smtp'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'YOUR_EMAIL_ADDRESS'
