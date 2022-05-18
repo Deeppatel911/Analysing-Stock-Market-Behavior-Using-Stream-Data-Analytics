066178d3517c4d08f57525d6c6990f468250b391
Deeppatel911 <deepjpatel0911@gmail.com>
2022-05-19 01:20:41 +0530

    Update settings.py

--- django web app/stock_market/stock_market/settings.py
+++ django web app/stock_market/stock_market/settings.py
@@ -141,4 +141,9 @@ LOGIN_URL = 'login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# Add password reset configuration here through smtp
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'YOUR_EMAIL_ADDRESS'
+EMAIL_HOST_PASSWORD = 'YOUR_EMAIL_PASSWORD'
